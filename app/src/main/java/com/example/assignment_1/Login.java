bb1af3923f5e244a59acc0771bd87827df7fa181
bcaba <bcabahadur@gmail.com>
2023-04-09 23:22:22 +0545

    final

---
+++ app/src/main/java/com/example/assignment_1/Login.java
@@ -0,0 +1,13 @@
+package com.example.assignment_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
